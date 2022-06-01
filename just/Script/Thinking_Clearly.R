ce8706c48226529247e52911864e0d6ece1a624a
aidar-zinnatullin <aidar.zinnatulla@gmail.com>
2022-06-01 11:36:33 +0200

    R sctrip changes

--- just/Script/Thinking_Clearly.R
+++ just/Script/Thinking_Clearly.R
@@ -1,7 +1,8 @@
 library(here)
 library(ggplot2)
 library(tidyverse)
-chapter_5_schooling_earn <- read_csv(here('Files for Data Exercises', 'SchoolingEarnings.csv'))
+here()
+chapter_5_schooling_earn <- read_csv(here('just', 'Files for Data Exercises', 'SchoolingEarnings.csv'))
 str(chapter_5_schooling_earn)
 
 #5.1
@@ -12,7 +13,7 @@ summary(reg_m1)
 ggplot(data = chapter_5_schooling_earn)+
   geom_point(mapping = aes(y = earnings, x = schooling))+
   theme_classic()
-ggsave(here('Images', 'Figure 1.jpeg'))
+ggsave(here('just','Images', 'Figure 1.jpeg'))
 
 #let's have a look at predicted values
 
@@ -24,7 +25,7 @@ ggplot(predicted_data) +
   geom_point(mapping = aes(y = predicted, x = observed)) +
   geom_abline(color = "green")+
   theme_classic()
-ggsave(here('Images', 'Figure 2.jpeg'))
+ggsave(here('just', 'Images', 'Figure 2.jpeg'))
 
 # polynomials
 reg_m2 <- lm(data = chapter_5_schooling_earn, earnings~schooling+I(schooling^2) + I(schooling^3)+ I(schooling^4))
@@ -35,11 +36,11 @@ ggplot(data=chapter_5_schooling_earn, aes(schooling,earnings)) +
   geom_point() + 
   geom_smooth(method="lm", formula=y~I(x^4)+I(x^3)+I(x^2)) +
   theme_classic()
-ggsave(here('Images', 'Figure 3.jpeg'))
+ggsave(here('just', 'Images', 'Figure 3.jpeg'))
 
 
 # base R
-jpeg(filename = here("Images", "Figure 4.jpeg"), width = 16, height = 8, units = 'in', res = 500)
+jpeg(filename = here("just", "Images", "Figure 4.jpeg"), width = 16, height = 8, units = 'in', res = 500)
 plot(x=chapter_5_schooling_earn$schooling, y=chapter_5_schooling_earn$earnings, pch=20, col="grey", xlab = 'Schooling', ylab = 'Earnings')
 
 lines(chapter_5_schooling_earn$schooling, predict(lm(earnings~schooling, data=chapter_5_schooling_earn)), type="l", col="orange1", lwd=2)
@@ -63,6 +64,7 @@ ggplot(data = chapter_5_schooling_earn, aes(x = schooling, y = earnings))+
   geom_point(aes(colour = school_completion))+
   geom_smooth(method=lm, aes(fill=school_completion), se=TRUE, fullrange=FALSE)+
   theme_classic()
-ggsave(here('Images', 'Figure 5.jpeg'))
+ggsave(here('just', 'Images', 'Figure 5.jpeg'))
 
 #### New analyses to be continued
+
